2c61b46d72e89a84e1080375133cb99186dbe2e2
YoungCeo <leonardo.tommasi@hotmail.com>
2025-04-11 11:05:15 +0200

    versione localhost funzionante

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`✅ Server in esecuzione su http://localhost:${port}`);
